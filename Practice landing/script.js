c23b0f8f8b060404f92a4852b8c1a2576a751081
Bita Behzadipour <69535161+BitaBehzadipour@users.noreply.github.com>
2026-07-24 19:38:29 +0330

    Add files via upload

---
+++ Practice landing/script.js
@@ -0,0 +1,8 @@
+'use strict';
+const nav=document.querySelector('nav');
+const menulink=document.querySelector('menu-link');
+nav.addEventListener('mouseenter',function(e){
+    const link=e.target;
+    const sibling=link.closest('nav').querySelector('menu-link');
+    if(link!==)
+})
